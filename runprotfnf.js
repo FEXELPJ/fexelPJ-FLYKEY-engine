c83e4352e19651114ab5554db263dee6fc9ab16e
FEXELPJ <102598153+FEXELPJ@users.noreply.github.com>
2022-03-29 20:29:53 +0300

    Update runprotfnf.js

--- runprotfnf.js
+++ runprotfnf.js
@@ -3,3 +3,9 @@ var setport = [name]FK
 if(setport = [name]FK){
   console.log('FEXEL.FK/RUNPORTFNF.JS IS FINISH');
 }
+
+///FEXEL.DATA///
+var ip = 129.*.*.*
+if(ip = 129.*.*.*){
+  console.log('FEXEL.FK/true ip IS FINISH');
+}
